577fd7d4a1fcf1dcecea83ae42a3737490cd29e9
aafiii-v <afinaufalrizkyyang24@gmail.com>
2024-12-22 13:45:10 +0700

    initial commit development

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -17,5 +17,10 @@ namespace TrainingProjectAPI.Services
             var datas = _context.Customers.ToList();
             return datas;
         }
+
+        public int GetCustomer()
+        {
+            return 1;
+        }
     }
 }
