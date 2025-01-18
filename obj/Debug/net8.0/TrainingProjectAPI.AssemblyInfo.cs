7b4168a9ac7f3ea18d019598ad9d15b0ee072c9f
aafiii-v <afinaufalrizkyyang24@gmail.com>
2025-01-18 15:44:03 +0700

    insert

--- obj/Debug/net8.0/TrainingProjectAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrainingProjectAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d81a29022f8785e39f58ed02e1fe8ebad3751d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a9a6fe355cc8a9c38930fc4a418f1bf21259ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
