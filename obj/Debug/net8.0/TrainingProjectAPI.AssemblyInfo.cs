ba483d1d254453a65551095c20b44c1414e2925a
aafiii-v <afinaufalrizkyyang24@gmail.com>
2025-01-26 10:52:54 +0700

    Add Changes

--- obj/Debug/net8.0/TrainingProjectAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrainingProjectAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1459bf9744328d38086662a409ba23519dad6d56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab83b28a152e7b2485f3847e3420dcbf5231d4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
